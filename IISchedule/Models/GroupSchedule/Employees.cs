1c7b3fed921d842dd85fde8316ccee6fdfb510e6
Kostya <betenya.kostya@mail.ru>
2024-08-06 18:30:52 +0300

    create Group Schedule Model

---
+++ IISchedule/Models/GroupSchedule/Employees.cs
@@ -0,0 +1,18 @@
+﻿namespace IISchedule.Models.GroupSchedule
+{
+    class Employees
+    {
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? middleName { get; set; }
+        public string? degree { get; set; }
+        public string? degreeAbbrev { get; set; }
+        public string? rank { get; set; }
+        public string? photoLink { get; set; }
+        public string? calendarId { get; set; }
+        public int id { get; set; }
+        public string? urlId { get; set; }
+        public string? email { get; set; }
+        public string? jobPositions { get; set; }
+    }
+}
---
+++ IISchedule/Models/GroupSchedule/Schedules.cs
@@ -0,0 +1,12 @@
+﻿namespace IISchedule.Models.GroupSchedule
+{
+    class Schedules
+    {
+        public Schedule? monday { get; set; }
+        public Schedule? tuesday { get; set; }
+        public Schedule? wednesday { get; set; }
+        public Schedule? thursday { get; set; }
+        public Schedule? friday { get; set; }
+        public Schedule? sunday { get; set; }
+    }
+}
