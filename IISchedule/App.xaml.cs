78f3535092dee91e4ec02dc2b57ce4ffb616a002
Kostya <betenya.kostya@mail.ru>
2024-08-06 13:16:23 +0300

    create empty MAUI App

---
+++ IISchedule/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IISchedule
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
